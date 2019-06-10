2129d0e183bcf66a682ce40b616348d199b7ba8c
belette06 <cyril.avronsart@gmail.com>
2019-06-10 03:14:02 +0200

    mailer ok welcome and comfirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,31 +5,52 @@
 # Table name: users
 #
 #  id                     :bigint(8)        not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  invitation_accepted_at :datetime
+#  invitation_created_at  :datetime
+#  invitation_limit       :integer
+#  invitation_sent_at     :datetime
+#  invitation_token       :string
+#  invited_by_type        :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  invited_by_id          :integer
 #
 # Indexes
 #
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
+#  index_users_on_invitation_token      (invitation_token) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
 class User < ApplicationRecord
+  after_create :welcome_send
+
+   has_one :proprietor
+   accepts_nested_attributes_for :proprietor
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
-  has_one :proprietor
-  accepts_nested_attributes_for :proprietor
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
   validates :password, length: { in: 6..20, on: :create }
+
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   root to: "static_pages#index"
 
+  devise_for :users, controllers: {
+                                              sessions: 'users/sessions',
+                                              confirmations: 'users/confirmations',
+                                              registrations: 'users/registrations'
+                                            }
+
+  resources :tenants
   resources :proprietors
   resources :homes do
     resources :appointments
+
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
