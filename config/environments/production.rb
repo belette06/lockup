8e439275df589a3432c317cc6f887888a9f2d080
belette06 <cyril.avronsart@gmail.com>
2019-08-02 21:12:20 +0200

    teste heroku production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -58,8 +58,6 @@ Rails.application.configure do
 
   # Use a different cache store in production.
    #config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
-  config.active_record.cache_versioning = false
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Homelockup::Application.config.session_store :redis_store,
-                                                servers: ["redis://localhost:6379/0/session"],
+                                                servers: ["redis://localhost:6380/0/session"],
                                                 expire_after: 90.minutes,
                                                 key: "_#{Rails.application.class.parent_name.downcase}_session",
                                                 threadsafe: false
