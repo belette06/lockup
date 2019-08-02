b3a7f34b55c7c6b28c9241158f52b5b8951329e9
belette06 <cyril.avronsart@gmail.com>
2019-08-02 18:46:49 +0200

    mja heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-MyApplication::Application.config.session_store :redis_store,
+Homelockup::Application.config.session_store :redis_store,
                                                 servers: ["redis://localhost:6379/0/session"],
                                                 expire_after: 90.minutes,
                                                 key: "_#{Rails.application.class.parent_name.downcase}_session",
